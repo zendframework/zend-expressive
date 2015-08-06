3c1f75bcd6a72df1572a6b9cb699484eddcfb0c5
Matthew Weier O'Phinney <matthew@zend.com>
2015-08-06 17:16:01 -0500

    Update all source/license links to point to this repo
    - s/diactoros/expressive/g

--- test/Container/ApplicationFactoryTest.php
+++ test/Container/ApplicationFactoryTest.php
@@ -2,9 +2,9 @@
 /**
  * Zend Framework (http://framework.zend.com/)
  *
- * @see       https://github.com/zendframework/zend-diactoros for the canonical source repository
+ * @see       https://github.com/zendframework/zend-expressive for the canonical source repository
  * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-diactoros/blob/master/LICENSE.md New BSD License
+ * @license   https://github.com/zendframework/zend-expressive/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\Expressive\Container;
