eeadd27dbab1dc1a2a7bbdbe418528d74a498204
Matthew Weier O'Phinney <matthew@zend.com>
2016-01-21 11:59:21 -0600

    Added documentation deployment toolchain
    This is the documentation deployment toolchain for generating gh-pages
    on-the-fly from the master branch.
    
    The Travis-CI configuration now defines several environment variables for
    specifying the repository, github username and email, and github token
    (encrypted) to use when committing the files.
    
    `doc/deploy.sh` performs the actual build process. It first initializes a bare
    repository under `doc/html/`, adds the repository as a remote, fetches it, and
    resets head to the gh-pages branch. It then builds the documentation, adds all
    changes, and pushes them back to the gh-pages branch.

---
+++ doc/deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Deploy documentation to gh-pages
+#
+# Environment variables that may be of use:
+#
+# - GH_USER_NAME indicates the GitHub author name to use;
+# - GH_USER_EMAIL indicates the email address for that author;
+# - GH_REF indicates the URI, without scheme or user-info, to the repository;
+# - GH_TOKEN is the personal security token to use for commits.
+#
+# All of the above are exported via the project .travis.yml file (with
+# GH_TOKEN being encrypted and present in the `secure` key). The user details
+# need to match the token used for this to work.
+
+set -o errexit -o nounset
+
+# Get curent commit revision
+rev=$(git rev-parse --short HEAD)
+
+# Path to bookdown
+BOOKDOWN=${HOME}/.composer/vendor/bin/bookdown
+
+# Get documentation templates and assets
+(
+    cd doc
+    git clone git://github.com/zendframework/documentation assets
+)
+
+mkdir -p doc/html
+
+# Initialize gh-pages checkout
+(
+    cd doc/html
+    git init
+    git config user.name "${GH_USER_NAME}"
+    git config user.email "${GH_USER_EMAIL}"
+    git remote add upstream "https://${GH_TOKEN}@${GH_REF}"
+    git fetch upstream
+    git reset upstream/gh-pages
+)
+
+# Build the documentation
+$BOOKDOWN doc/bookdown.json --template=doc/assets/doc/templates/main.php
+
+# Commit and push the documentation to gh-pages
+(
+    cd doc/html
+    touch .
+    git add -A .
+    git commit -m "Rebuild pages at ${rev}"
+    git push upstream HEAD:gh-pages
+)
