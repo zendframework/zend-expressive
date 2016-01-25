05fa3523731500067aff426cebdb68130bc54be6
Matthew Weier O'Phinney <matthew@zend.com>
2016-01-25 09:08:28 -0600

    Use zf-mkdoc-theme 0.1.6
    Fixes navbar collapse issues, and adds a pager to the top of content whenever
    the navbar is collapsed.

--- doc/build.sh
+++ doc/build.sh
@@ -14,7 +14,7 @@
 
 # Get documentation templates and assets
 if [[ ! -d zf-mkdoc-theme ]];then
-    wget -O zf-mkdoc-theme.tgz https://github.com/weierophinney/zf-mkdoc-theme/archive/0.1.2.tar.gz ;
+    wget -O zf-mkdoc-theme.tgz https://github.com/weierophinney/zf-mkdoc-theme/archive/0.1.6.tar.gz ;
     mkdir zf-mkdoc-theme ;
     (
         cd zf-mkdoc-theme ;
