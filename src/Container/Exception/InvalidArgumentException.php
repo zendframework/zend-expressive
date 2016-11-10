dd2d6cbcfac46b52f62e8a3cf9310c74b2d901d5
Matthew Weier O'Phinney <matthew@zend.com>
2016-11-10 13:28:32 -0600

    Header does not need the initial lines as originally proposed
    Updated the template to remove the initial two lines, and then ran it.
    Used the following command to edit each:
    
    ```bash
    vim $(composer license-check 2>&1 | grep -P '^-\>' | cut -d ' ' -f 2 | sed -e 's#^#src/#')
    ```
    
    And then searched across all buffers and removed lines 3-4.

--- src/Container/Exception/InvalidArgumentException.php
+++ src/Container/Exception/InvalidArgumentException.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
  * @see       https://github.com/zendframework/zend-expressive for the canonical source repository
  * @copyright Copyright (c) 2015-2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive/blob/master/LICENSE.md New BSD License
