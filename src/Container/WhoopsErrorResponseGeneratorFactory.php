c85495b882b0afa9dd2079559daed38ae256e7df
Matthew Weier O'Phinney <matthew@zend.com>
2016-11-11 16:31:27 -0600

    Added WhoopsErrorResponseGeneratorFactory
    Factory for generating and returning a WhoopsErrorResponseGenerator.

---
+++ src/Container/WhoopsErrorResponseGeneratorFactory.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-expressive for the canonical source repository
+ * @copyright Copyright (c) 2016-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Expressive\Container;
+
+use Interop\Container\ContainerInterface;
+use Zend\Expressive\Middleware\WhoopsErrorResponseGenerator;
+
+class WhoopsErrorResponseGeneratorFactory
+{
+    /**
+     * @param ContainerInterface $container
+     * @return WhoopsErrorResponseGenerator
+     */
+    public function __invoke(ContainerInterface $container)
+    {
+        return new WhoopsErrorResponseGenerator(
+            $container->get('Zend\Expressive\Whoops')
+        );
+    }
+}
