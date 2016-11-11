cfffdc75b0b2d8edf7889169e16b97c8c700a23a
Matthew Weier O'Phinney <matthew@zend.com>
2016-11-11 16:56:20 -0600

    More docheader changes
    - `s/@link/@see/g`

--- src/Middleware/WhoopsErrorResponseGenerator.php
+++ src/Middleware/WhoopsErrorResponseGenerator.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link      http://github.com/zendframework/zend-expressive for the canonical source repository
+ * @see       https://github.com/zendframework/zend-expressive for the canonical source repository
  * @copyright Copyright (c) 2015-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @license   https://github.com/zendframework/zend-expressive/blob/master/LICENSE.md New BSD License
  */
 
 namespace Zend\Expressive\Middleware;
