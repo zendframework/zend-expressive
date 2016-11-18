4f1b352fbad114c6b80a4678490b7390cd511dd7
Matthew Weier O'Phinney <matthew@zend.com>
2016-11-18 11:14:30 -0600

    Allow copyright range to be only the current year
    - Per @webimpress

--- src/ApplicationConfigInjectionTrait.php
+++ src/ApplicationConfigInjectionTrait.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive for the canonical source repository
- * @copyright Copyright (c) 2015-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive/blob/master/LICENSE.md New BSD License
  */
 
