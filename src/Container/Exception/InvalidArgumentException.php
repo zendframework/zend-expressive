ccf234608aa22825cfa43725dc0a8063caa65ba2
Matthew Weier O'Phinney <matthew@zend.com>
2017-01-26 16:55:48 -0600

    Deprecated container-specific exceptions

--- src/Container/Exception/InvalidArgumentException.php
+++ src/Container/Exception/InvalidArgumentException.php
@@ -9,6 +9,12 @@ namespace Zend\Expressive\Container\Exception;
 
 use Interop\Container\Exception\ContainerException;
 
+/**
+ * @deprecated since 1.1.0; to remove in 2.0.0. This exception is currently
+ *     thrown by `Zend\Expressive\Container\ApplicationFactory`; starting
+ *     in 2.0.0, that factory will instead throw
+ *     `Zend\Expressive\Exception\InvalidArgumentException`.
+ */
 class InvalidArgumentException extends \InvalidArgumentException implements
     ContainerException,
     ExceptionInterface
