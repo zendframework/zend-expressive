cb9ff50cc2ae5cf1fa45a0fdc0a0e8139e13fe20
Matthew Weier O'Phinney <matthew@zend.com>
2017-01-26 17:11:26 -0600

    Remove unused use statements
    - usage of these classes was removed when extracting the
      `LazyLoadingMiddleware`.

--- src/MarshalMiddlewareTrait.php
+++ src/MarshalMiddlewareTrait.php
@@ -7,9 +7,7 @@
 
 namespace Zend\Expressive;
 
-use Closure;
 use Interop\Container\ContainerInterface;
-use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface as ServerMiddlewareInterface;
 use Psr\Http\Message\ResponseInterface;
 use Zend\Stratigility\Delegate\CallableDelegateDecorator;
