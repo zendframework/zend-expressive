9809bc490e1409cba69f8f2ec276a8fec302fb90
Matthew Weier O'Phinney <matthew@zend.com>
2018-01-24 14:00:41 -0600

    Fixes license docblocks to conform to template

--- src/Container/DispatchMiddlewareFactory.php
+++ src/Container/DispatchMiddlewareFactory.php
@@ -4,6 +4,7 @@
  * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive/blob/master/LICENSE.md New BSD License
  */
+
 declare(strict_types=1);
 
 namespace Zend\Expressive\Container;
