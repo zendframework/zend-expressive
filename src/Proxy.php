cb597cddae9f9f5e933b4b1e8a11a0feae10a542
Matthew Weier O'Phinney <matthew@zend.com>
2018-01-30 15:30:42 -0600

    Created proxy class
    `Proxy` is a facade in front of the middleware factory, middleware pipe,
    route middleware, and application runner it composes. It essentially
    provides the original public API of `Application`.

---
+++ src/Proxy.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive for the canonical source repository
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Zend\Expressive;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Stratigility\MiddlewarePipeInterface;
+
+use function Zend\Stratigility\path;
+
+class Proxy implements MiddlewareInterface, RequestHandlerInterface
+{
+    /**
+     * @var MiddlewareFactory
+     */
+    private $factory;
+
+    /**
+     * @var MiddlewarePipeInterface
+     */
+    private $pipeline;
+
+    /**
+     * @var Middleware\RouteMiddleware
+     */
+    private $routes;
+
+    /**
+     * @var ApplicationRunner
+     */
+    private $runner;
+
+    public function __construct(
+        MiddlewareFactory $factory,
+        MiddlewarePipeInterface $pipeline,
+        Middleware\RouteMiddleware $routes,
+        ApplicationRunner $runner
+    ) {
+        $this->factory = $factory;
+        $this->pipeline = $pipeline;
+        $this->routes = $routes;
+        $this->runner = $runner;
+    }
+
+    /**
+     * Proxies to composed pipeline to handle.
+     * {@inheritDocs}
+     */
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        return $this->pipeline->handle($request);
+    }
+
+    /**
+     * Proxies to composed pipeline to process.
+     * {@inheritDocs}
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface
+    {
+        return $this->pipeline->process($request, $handler);
+    }
+
+    /**
+     * Run the application.
+     *
+     * Proxies to the ApplicationRunner::run() method.
+     */
+    public function run(ServerRequestInterface $request = null) : void
+    {
+        $this->runner->run($request);
+    }
+
+    /**
+     * Pipe middleware to the pipeline.
+     *
+     * If two arguments are present, they are passed to pipe(), after first
+     * passing the second argument to the factory's prepare() method.
+     *
+     * If only one argument is presented, it is passed to the factory prepare()
+     * method.
+     *
+     * The resulting middleware, in both cases, is piped to the pipeline.
+     *
+     * @param string|array|callable|MiddlewareInterface $middlewareOrPath
+     *     Either the middleware to pipe, or the path to segregate the $middleware
+     *     by, via a PathMiddlewareDecorator.
+     * @param null|string|array|callable|MiddlewareInterface $middleware If present,
+     *     middleware to segregate by the path specified in $middlewareOrPath.
+     */
+    public function pipe($middlewareOrPath, $middleware = null) : void
+    {
+        $middleware = $middleware === null
+            ? $this->factory->prepare($middlewareOrPath)
+            : path($middlewareOrPath, $this->factory->prepare($middleware));
+
+        $this->pipeline->pipe($middleware);
+    }
+
+    /**
+     * Add a route for the route middleware to match.
+     *
+     * @param string|array|callable|MiddlewareInterface $middleware Middleware
+     *     (or middleware service name) to associate with route.
+     * @param null|array $methods HTTP method to accept; null indicates any.
+     * @param null|string $name The name of the route.
+     */
+    public function route(string $path, $middleware, array $methods = null, string $name = null) : Router\Route
+    {
+        return $this->routes->route(
+            $path,
+            $this->factory->prepare($middleware),
+            $methods,
+            $name
+        );
+    }
+
+    /**
+     * @param string|array|callable|MiddlewareInterface $middleware Middleware
+     *     (or middleware service name) to associate with route.
+     * @param null|string $name The name of the route.
+     */
+    public function get(string $path, $middleware, string $name = null) : Router\Route
+    {
+        return $this->route($path, $middleware, ['GET'], $name);
+    }
+
+    /**
+     * @param string|array|callable|MiddlewareInterface $middleware Middleware
+     *     (or middleware service name) to associate with route.
+     * @param null|string $name The name of the route.
+     */
+    public function post(string $path, $middleware, $name = null) : Router\Route
+    {
+        return $this->route($path, $middleware, ['POST'], $name);
+    }
+
+    /**
+     * @param string|array|callable|MiddlewareInterface $middleware Middleware
+     *     (or middleware service name) to associate with route.
+     * @param null|string $name The name of the route.
+     */
+    public function put(string $path, $middleware, string $name = null) : Router\Route
+    {
+        return $this->route($path, $middleware, ['PUT'], $name);
+    }
+
+    /**
+     * @param string|array|callable|MiddlewareInterface $middleware Middleware
+     *     (or middleware service name) to associate with route.
+     * @param null|string $name The name of the route.
+     */
+    public function patch(string $path, $middleware, string $name = null) : Router\Route
+    {
+        return $this->route($path, $middleware, ['PATCH'], $name);
+    }
+
+    /**
+     * @param string|array|callable|MiddlewareInterface $middleware Middleware
+     *     (or middleware service name) to associate with route.
+     * @param null|string $name The name of the route.
+     */
+    public function delete(string $path, $middleware, string $name = null) : Router\Route
+    {
+        return $this->route($path, $middleware, ['DELETE'], $name);
+    }
+
+    /**
+     * @param string|array|callable|MiddlewareInterface $middleware Middleware
+     *     (or middleware service name) to associate with route.
+     * @param null|string $name The name of the route.
+     */
+    public function any(string $path, $middleware, string $name = null) : Router\Route
+    {
+        return $this->route($path, $middleware, null, $name);
+    }
+
+    /**
+     * Retrieve all directly registered routes with the application.
+     *
+     * @return Router\Route[]
+     */
+    public function getRoutes() : array
+    {
+        return $this->routes->getRoutes();
+    }
+}
---
+++ test/ProxyTest.php
@@ -0,0 +1,338 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive for the canonical source repository
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace ZendTest\Expressive;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Expressive\ApplicationRunner;
+use Zend\Expressive\MiddlewareFactory;
+use Zend\Expressive\Middleware\RouteMiddleware;
+use Zend\Expressive\Proxy;
+use Zend\Expressive\Router\Route;
+use Zend\Stratigility\MiddlewarePipe;
+use Zend\Stratigility\MiddlewarePipeInterface;
+use Zend\Stratigility\Middleware\PathMiddlewareDecorator;
+
+class ProxyTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->factory = $this->prophesize(MiddlewareFactory::class);
+        $this->pipeline = $this->prophesize(MiddlewarePipeInterface::class);
+        $this->routes = $this->prophesize(RouteMiddleware::class);
+        $this->runner = $this->prophesize(ApplicationRunner::class);
+
+        $this->proxy = new Proxy(
+            $this->factory->reveal(),
+            $this->pipeline->reveal(),
+            $this->routes->reveal(),
+            $this->runner->reveal()
+        );
+    }
+
+    public function createMockMiddleware()
+    {
+        return $this->prophesize(MiddlewareInterface::class)->reveal();
+    }
+
+    public function testHandleProxiesToPipelineToHandle()
+    {
+        $request = $this->prophesize(ServerRequestInterface::class)->reveal();
+        $response = $this->prophesize(ResponseInterface::class)->reveal();
+
+        $this->pipeline->handle($request)->willReturn($response);
+
+        $this->assertSame($response, $this->proxy->handle($request));
+    }
+
+    public function testProcessProxiesToPipelineToProcess()
+    {
+        $request = $this->prophesize(ServerRequestInterface::class)->reveal();
+        $response = $this->prophesize(ResponseInterface::class)->reveal();
+        $handler = $this->prophesize(RequestHandlerInterface::class)->reveal();
+
+        $this->pipeline->process($request, $handler)->willReturn($response);
+
+        $this->assertSame($response, $this->proxy->process($request, $handler));
+    }
+
+    public function testRunProxiesToRunner()
+    {
+        $this->runner->run(null)->shouldBeCalled();
+        $this->assertNull($this->proxy->run());
+    }
+
+    public function testRunProxiesToRunnerWithRequest()
+    {
+        $request = $this->prophesize(ServerRequestInterface::class)->reveal();
+        $this->runner->run($request)->shouldBeCalled();
+        $this->assertNull($this->proxy->run($request));
+    }
+
+    public function validMiddleware() : iterable
+    {
+        // @codingStandardsIgnoreStart
+        yield 'string'   => ['service'];
+        yield 'array'    => [['middleware', 'service']];
+        yield 'callable' => [function ($request, $response) {}];
+        yield 'instance' => [new MiddlewarePipe()];
+        // @codingStandardsIgnoreEnd
+    }
+
+    /**
+     * @dataProvider validMiddleware
+     * @param string|array|callable|MiddlewareInterface $middleware
+     */
+    public function testPipeCanAcceptSingleMiddlewareArgument($middleware)
+    {
+        $preparedMiddleware = $this->createMockMiddleware();
+        $this->factory
+            ->prepare($middleware)
+            ->willReturn($preparedMiddleware);
+
+        $this->pipeline
+            ->pipe(Argument::that(function ($test) use ($preparedMiddleware) {
+                Assert::assertSame($preparedMiddleware, $test);
+                return $test;
+            }))
+            ->shouldBeCalled();
+
+        $this->assertNull($this->proxy->pipe($middleware));
+    }
+
+    /**
+     * @dataProvider validMiddleware
+     * @param string|array|callable|MiddlewareInterface $middleware
+     */
+    public function testPipeCanAcceptAPathArgument($middleware)
+    {
+        $preparedMiddleware = $this->createMockMiddleware();
+        $this->factory
+            ->prepare($middleware)
+            ->willReturn($preparedMiddleware);
+
+        $this->pipeline
+            ->pipe(Argument::that(function ($test) use ($preparedMiddleware) {
+                Assert::assertInstanceOf(PathMiddlewareDecorator::class, $test);
+                Assert::assertAttributeSame('/foo', 'prefix', $test);
+                Assert::assertAttributeSame($preparedMiddleware, 'middleware', $test);
+                return $test;
+            }))
+            ->shouldBeCalled();
+
+        $this->assertNull($this->proxy->pipe('/foo', $middleware));
+    }
+
+    /**
+     * @dataProvider validMiddleware
+     * @param string|array|callable|MiddlewareInterface $middleware
+     */
+    public function testRouteAcceptsPathAndMiddlewareOnly($middleware)
+    {
+        $preparedMiddleware = $this->createMockMiddleware();
+
+        $this->factory
+            ->prepare($middleware)
+            ->willReturn($preparedMiddleware);
+
+        $route = $this->prophesize(Route::class)->reveal();
+
+        $this->routes
+            ->route(
+                '/foo',
+                $preparedMiddleware,
+                null,
+                null
+            )
+            ->willReturn($route);
+
+        $this->assertSame($route, $this->proxy->route('/foo', $middleware));
+    }
+
+    /**
+     * @dataProvider validMiddleware
+     * @param string|array|callable|MiddlewareInterface $middleware
+     */
+    public function testRouteAcceptsPathMiddlewareAndMethodsOnly($middleware)
+    {
+        $preparedMiddleware = $this->createMockMiddleware();
+
+        $this->factory
+            ->prepare($middleware)
+            ->willReturn($preparedMiddleware);
+
+        $route = $this->prophesize(Route::class)->reveal();
+
+        $this->routes
+            ->route(
+                '/foo',
+                $preparedMiddleware,
+                ['GET', 'POST'],
+                null
+            )
+            ->willReturn($route);
+
+        $this->assertSame($route, $this->proxy->route('/foo', $middleware, ['GET', 'POST']));
+    }
+
+    /**
+     * @dataProvider validMiddleware
+     * @param string|array|callable|MiddlewareInterface $middleware
+     */
+    public function testRouteAcceptsPathMiddlewareMethodsAndName($middleware)
+    {
+        $preparedMiddleware = $this->createMockMiddleware();
+
+        $this->factory
+            ->prepare($middleware)
+            ->willReturn($preparedMiddleware);
+
+        $route = $this->prophesize(Route::class)->reveal();
+
+        $this->routes
+            ->route(
+                '/foo',
+                $preparedMiddleware,
+                ['GET', 'POST'],
+                'foo'
+            )
+            ->willReturn($route);
+
+        $this->assertSame($route, $this->proxy->route('/foo', $middleware, ['GET', 'POST'], 'foo'));
+    }
+
+    public function requestMethodsWithValidMiddleware() : iterable
+    {
+        foreach (['get', 'post', 'put', 'patch', 'delete'] as $method) {
+            foreach ($this->validMiddleware() as $key => $data) {
+                array_unshift($data, $method);
+                $name = sprintf('%s-%s', $method, $key);
+                yield $name => $data;
+            }
+        }
+    }
+
+    /**
+     * @dataProvider requestMethodsWithValidMiddleware
+     * @param string|array|callable|MiddlewareInterface $middleware
+     */
+    public function testSpecificRouteMethodsCanAcceptOnlyPathAndMiddleware(string $method, $middleware)
+    {
+        $preparedMiddleware = $this->createMockMiddleware();
+
+        $this->factory
+            ->prepare($middleware)
+            ->willReturn($preparedMiddleware);
+
+        $route = $this->prophesize(Route::class)->reveal();
+
+        $this->routes
+            ->route(
+                '/foo',
+                $preparedMiddleware,
+                [strtoupper($method)],
+                null
+            )
+            ->willReturn($route);
+
+        $this->assertSame($route, $this->proxy->{$method}('/foo', $middleware));
+    }
+
+    /**
+     * @dataProvider requestMethodsWithValidMiddleware
+     * @param string|array|callable|MiddlewareInterface $middleware
+     */
+    public function testSpecificRouteMethodsCanAcceptPathMiddlewareAndName(string $method, $middleware)
+    {
+        $preparedMiddleware = $this->createMockMiddleware();
+
+        $this->factory
+            ->prepare($middleware)
+            ->willReturn($preparedMiddleware);
+
+        $route = $this->prophesize(Route::class)->reveal();
+
+        $this->routes
+            ->route(
+                '/foo',
+                $preparedMiddleware,
+                [strtoupper($method)],
+                'foo'
+            )
+            ->willReturn($route);
+
+        $this->assertSame($route, $this->proxy->{$method}('/foo', $middleware, 'foo'));
+    }
+
+    /**
+     * @dataProvider validMiddleware
+     * @param string|array|callable|MiddlewareInterface $middleware
+     */
+    public function testAnyMethodPassesNullForMethodWhenNoNamePresent($middleware)
+    {
+        $preparedMiddleware = $this->createMockMiddleware();
+
+        $this->factory
+            ->prepare($middleware)
+            ->willReturn($preparedMiddleware);
+
+        $route = $this->prophesize(Route::class)->reveal();
+
+        $this->routes
+            ->route(
+                '/foo',
+                $preparedMiddleware,
+                null,
+                null
+            )
+            ->willReturn($route);
+
+        $this->assertSame($route, $this->proxy->any('/foo', $middleware));
+    }
+
+    /**
+     * @dataProvider validMiddleware
+     * @param string|array|callable|MiddlewareInterface $middleware
+     */
+    public function testAnyMethodPassesNullForMethodWhenAllArgumentsPresent($middleware)
+    {
+        $preparedMiddleware = $this->createMockMiddleware();
+
+        $this->factory
+            ->prepare($middleware)
+            ->willReturn($preparedMiddleware);
+
+        $route = $this->prophesize(Route::class)->reveal();
+
+        $this->routes
+            ->route(
+                '/foo',
+                $preparedMiddleware,
+                null,
+                'foo'
+            )
+            ->willReturn($route);
+
+        $this->assertSame($route, $this->proxy->any('/foo', $middleware, 'foo'));
+    }
+
+    public function testGetRoutesProxiesToRouteMiddleware()
+    {
+        $route = $this->prophesize(Route::class)->reveal();
+        $this->routes->getRoutes()->willReturn([$route]);
+
+        $this->assertSame([$route], $this->proxy->getRoutes());
+    }
+}
