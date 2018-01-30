e08102c6cd96bdd8e30a5e398ed8794e452619d7
Matthew Weier O'Phinney <matthew@zend.com>
2018-01-30 11:32:09 -0600

    Add route registration capabilities to RouteMiddleware
    Adds the following methods from `Application` to `RouteMiddleware`:
    
    - `route()`
    - `get()`
    - `post()`
    - `patch()`
    - `put()`
    - `delete()`
    - `any()`
    - `getRoutes()`
    
    For all methods that accept middleware, the `$middleware` argument is
    now strongly typed against the PSR-15 `MiddlewareInterface`.

--- src/Middleware/RouteMiddleware.php
+++ src/Middleware/RouteMiddleware.php
@@ -14,6 +14,8 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Expressive\Exception;
+use Zend\Expressive\Router\Route;
 use Zend\Expressive\Router\RouteResult;
 use Zend\Expressive\Router\RouterInterface;
 
@@ -46,6 +48,13 @@ class RouteMiddleware implements MiddlewareInterface
      */
     private $router;
 
+    /**
+     * List of all routes registered directly with the application.
+     *
+     * @var Route[]
+     */
+    private $routes = [];
+
     public function __construct(RouterInterface $router, ResponseInterface $responsePrototype)
     {
         $this->router = $router;
@@ -71,5 +80,128 @@ class RouteMiddleware implements MiddlewareInterface
         }
 
         return $handler->handle($request);
+    }
+
+    /**
+     * Add a route for the route middleware to match.
+     *
+     * Accepts either a Route instance, or a combination of a path and
+     * middleware, and optionally the HTTP methods allowed.
+     *
+     * @param null|array $methods HTTP method to accept; null indicates any.
+     * @param null|string $name The name of the route.
+     * @throws Exception\InvalidArgumentException if $path is not a Route AND middleware is null.
+     * @throws Exception\InvalidMiddlewareException if $middleware is neither a
+     *     string nor a MiddlewareInterface instance.
+     */
+    public function route(
+        string $path,
+        MiddlewareInterface $middleware,
+        array $methods = null,
+        string $name = null
+    ) : Route {
+        $this->checkForDuplicateRoute($path, $methods);
+
+        $methods = null === $methods ? Route::HTTP_METHOD_ANY : $methods;
+        $route   = new Route($path, $middleware, $methods, $name);
+
+        $this->routes[] = $route;
+        $this->router->addRoute($route);
+
+        return $route;
+    }
+
+    /**
+     * @param null|string $name The name of the route.
+     */
+    public function get(string $path, MiddlewareInterface $middleware, string $name = null) : Route
+    {
+        return $this->route($path, $middleware, ['GET'], $name);
+    }
+
+    /**
+     * @param null|string $name The name of the route.
+     */
+    public function post(string $path, MiddlewareInterface $middleware, string $name = null) : Route
+    {
+        return $this->route($path, $middleware, ['POST'], $name);
+    }
+
+    /**
+     * @param null|string $name The name of the route.
+     */
+    public function put(string $path, MiddlewareInterface $middleware, string $name = null) : Route
+    {
+        return $this->route($path, $middleware, ['PUT'], $name);
+    }
+
+    /**
+     * @param null|string $name The name of the route.
+     */
+    public function patch(string $path, MiddlewareInterface $middleware, string $name = null) : Route
+    {
+        return $this->route($path, $middleware, ['PATCH'], $name);
+    }
+
+    /**
+     * @param null|string $name The name of the route.
+     */
+    public function delete(string $path, MiddlewareInterface $middleware, string $name = null) : Route
+    {
+        return $this->route($path, $middleware, ['DELETE'], $name);
+    }
+
+    /**
+     * @param null|string $name The name of the route.
+     */
+    public function any(string $path, MiddlewareInterface $middleware, string $name = null) : Route
+    {
+        return $this->route($path, $middleware, null, $name);
+    }
+
+    /**
+     * Retrieve all directly registered routes with the application.
+     *
+     * @return Route[]
+     */
+    public function getRoutes() : array
+    {
+        return $this->routes;
+    }
+
+    /**
+     * Determine if the route is duplicated in the current list.
+     *
+     * Checks if a route with the same name or path exists already in the list;
+     * if so, and it responds to any of the $methods indicated, raises
+     * a DuplicateRouteException indicating a duplicate route.
+     *
+     * @throws Exception\DuplicateRouteException on duplicate route detection.
+     */
+    private function checkForDuplicateRoute(string $path, array $methods = null) : void
+    {
+        if (null === $methods) {
+            $methods = Route::HTTP_METHOD_ANY;
+        }
+
+        $matches = array_filter($this->routes, function (Route $route) use ($path, $methods) {
+            if ($path !== $route->getPath()) {
+                return false;
+            }
+
+            if ($methods === Route::HTTP_METHOD_ANY) {
+                return true;
+            }
+
+            return array_reduce($methods, function ($carry, $method) use ($route) {
+                return ($carry || $route->allowsMethod($method));
+            }, false);
+        });
+
+        if (! empty($matches)) {
+            throw new Exception\DuplicateRouteException(
+                'Duplicate route detected; same name or path, and one or more HTTP methods intersect'
+            );
+        }
     }
 }
--- test/Middleware/RouteMiddlewareTest.php
+++ test/Middleware/RouteMiddlewareTest.php
@@ -11,15 +11,22 @@ namespace ZendTest\Expressive\Middleware;
 
 use Fig\Http\Message\StatusCodeInterface as StatusCode;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
 use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use TypeError;
+use Zend\Expressive\Exception;
 use Zend\Expressive\Middleware\RouteMiddleware;
+use Zend\Expressive\MiddlewareFactory;
 use Zend\Expressive\Router\Route;
 use Zend\Expressive\Router\RouteResult;
 use Zend\Expressive\Router\RouterInterface;
+use ZendTest\Expressive\TestAsset\InteropMiddleware;
+
+use function Zend\Stratigility\middleware;
 
 class RouteMiddlewareTest extends TestCase
 {
@@ -49,6 +56,18 @@ class RouteMiddlewareTest extends TestCase
 
         $this->request  = $this->prophesize(ServerRequestInterface::class);
         $this->handler = $this->prophesize(RequestHandlerInterface::class);
+        $this->noopMiddleware = new InteropMiddleware();
+    }
+
+    public function commonHttpMethods()
+    {
+        return [
+            'GET'    => ['GET'],
+            'POST'   => ['POST'],
+            'PUT'    => ['PUT'],
+            'PATCH'  => ['PATCH'],
+            'DELETE' => ['DELETE'],
+        ];
     }
 
     public function testRoutingFailureDueToHttpMethodCallsNextWithNotAllowedResponseAndError()
@@ -109,5 +128,124 @@ class RouteMiddlewareTest extends TestCase
 
         $response = $this->middleware->process($this->request->reveal(), $this->handler->reveal());
         $this->assertSame($expected, $response);
+    }
+
+    public function testRouteMethodReturnsRouteInstance()
+    {
+        $this->router->addRoute(Argument::type(Route::class))->shouldBeCalled();
+        $route = $this->middleware->route('/foo', $this->noopMiddleware);
+        $this->assertInstanceOf(Route::class, $route);
+        $this->assertEquals('/foo', $route->getPath());
+        $this->assertSame($this->noopMiddleware, $route->getMiddleware());
+    }
+
+    public function testAnyRouteMethod()
+    {
+        $this->router->addRoute(Argument::type(Route::class))->shouldBeCalled();
+        $route = $this->middleware->any('/foo', $this->noopMiddleware);
+        $this->assertInstanceOf(Route::class, $route);
+        $this->assertEquals('/foo', $route->getPath());
+        $this->assertSame($this->noopMiddleware, $route->getMiddleware());
+        $this->assertSame(Route::HTTP_METHOD_ANY, $route->getAllowedMethods());
+    }
+
+    /**
+     * @dataProvider commonHttpMethods
+     *
+     * @param string $method
+     */
+    public function testCanCallRouteWithHttpMethods($method)
+    {
+        $this->router->addRoute(Argument::type(Route::class))->shouldBeCalled();
+        $route = $this->middleware->route('/foo', $this->noopMiddleware, [$method]);
+        $this->assertInstanceOf(Route::class, $route);
+        $this->assertEquals('/foo', $route->getPath());
+        $this->assertSame($this->noopMiddleware, $route->getMiddleware());
+        $this->assertTrue($route->allowsMethod($method));
+        $this->assertSame([$method], $route->getAllowedMethods());
+    }
+
+    public function testCanCallRouteWithMultipleHttpMethods()
+    {
+        $this->router->addRoute(Argument::type(Route::class))->shouldBeCalled();
+        $methods = array_keys($this->commonHttpMethods());
+        $route = $this->middleware->route('/foo', $this->noopMiddleware, $methods);
+        $this->assertInstanceOf(Route::class, $route);
+        $this->assertEquals('/foo', $route->getPath());
+        $this->assertSame($this->noopMiddleware, $route->getMiddleware());
+        $this->assertSame($methods, $route->getAllowedMethods());
+    }
+
+    public function testCallingRouteWithExistingPathAndOmittingMethodsArgumentRaisesException()
+    {
+        $this->router->addRoute(Argument::type(Route::class))->shouldBeCalledTimes(2);
+        $this->middleware->route('/foo', $this->noopMiddleware);
+        $this->middleware->route('/bar', $this->noopMiddleware);
+        $this->expectException(Exception\DuplicateRouteException::class);
+        $this->middleware->route('/foo', middleware(function ($request, $handler) {
+        }));
+    }
+
+    public function invalidPathTypes()
+    {
+        return [
+            'null'       => [null],
+            'true'       => [true],
+            'false'      => [false],
+            'zero'       => [0],
+            'int'        => [1],
+            'zero-float' => [0.0],
+            'float'      => [1.1],
+            'array'      => [['path' => 'route']],
+            'object'     => [(object) ['path' => 'route']],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidPathTypes
+     *
+     * @param mixed $path
+     */
+    public function testCallingRouteWithAnInvalidPathTypeRaisesAnException($path)
+    {
+        $this->expectException(TypeError::class);
+        $this->middleware->route($path, new InteropMiddleware());
+    }
+
+    /**
+     * @dataProvider commonHttpMethods
+     *
+     * @param mixed $method
+     */
+    public function testCommonHttpMethodsAreExposedAsClassMethodsAndReturnRoutes($method)
+    {
+        $route = $this->middleware->{$method}('/foo', $this->noopMiddleware);
+        $this->assertInstanceOf(Route::class, $route);
+        $this->assertEquals('/foo', $route->getPath());
+        $this->assertSame($this->noopMiddleware, $route->getMiddleware());
+        $this->assertEquals([$method], $route->getAllowedMethods());
+    }
+
+    public function testCreatingHttpRouteMethodWithExistingPathButDifferentMethodCreatesNewRouteInstance()
+    {
+        $this->router->addRoute(Argument::type(Route::class))->shouldBeCalledTimes(2);
+        $route = $this->middleware->route('/foo', $this->noopMiddleware, []);
+
+        $middleware = new InteropMiddleware();
+        $test = $this->middleware->get('/foo', $middleware);
+        $this->assertNotSame($route, $test);
+        $this->assertSame($route->getPath(), $test->getPath());
+        $this->assertSame(['GET'], $test->getAllowedMethods());
+        $this->assertSame($middleware, $test->getMiddleware());
+    }
+
+    public function testCreatingHttpRouteWithExistingPathAndMethodRaisesException()
+    {
+        $this->router->addRoute(Argument::type(Route::class))->shouldBeCalledTimes(1);
+        $this->middleware->get('/foo', $this->noopMiddleware);
+
+        $this->expectException(Exception\DuplicateRouteException::class);
+        $this->middleware->get('/foo', middleware(function ($request, $handler) {
+        }));
     }
 }
