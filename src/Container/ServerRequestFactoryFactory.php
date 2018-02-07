9daeae690d3c09054a1a64f442edad63958c91c2
Matthew Weier O'Phinney <matthew@zend.com>
2018-02-07 08:14:41 -0600

    Make the ServerRequestFactory service compatible with all containers
    We cannot return just `ServerRequestFactory::fromGlobals` or
    `[ServerRequestFactory::class, 'fromGlobals']` as not all containers
    allow vanilla PHP callable services. Instead, we wrap it in an
    anonymous function here, which is allowed by all containers tested
    at this time.

--- src/Container/ServerRequestFactoryFactory.php
+++ src/Container/ServerRequestFactoryFactory.php
@@ -12,10 +12,21 @@ namespace Zend\Expressive\Container;
 use Psr\Container\ContainerInterface;
 use Zend\Diactoros\ServerRequestFactory;
 
+/**
+ * Return a factory for generating a server request.
+ *
+ * We cannot return just `ServerRequestFactory::fromGlobals` or
+ * `[ServerRequestFactory::class, 'fromGlobals']` as not all containers
+ * allow vanilla PHP callable services. Instead, we wrap it in an
+ * anonymous function here, which is allowed by all containers tested
+ * at this time.
+ */
 class ServerRequestFactoryFactory
 {
     public function __invoke(ContainerInterface $container) : callable
     {
-        return [ServerRequestFactory::class, 'fromGlobals'];
+        return function () {
+            return ServerRequestFactory::fromGlobals();
+        };
     }
 }
